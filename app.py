f900c2c7a30ccbdd96b44c635c00e7329058fc93
rwfilice <rwfilice@users.noreply.github.com>
2018-02-27 16:24:18 -0500

    Add files via upload
    Modified code to:
    1) Store data in individual text files corresponding with each image file in KITTI format for easy use with detectnet
    1a) If you include the string "-trunc-" in your annotation label e.g. "person-trunc-0.3" the file writer/reader will parse out the percent truncation (0.3 in this example) and store it properly in KITTI format; again optimized for detectnet
    2) This also facilitated activating the previous button and loading existing annotations as you tab back and forth in your image set
    3) Increased annotation font size

--- app.py
+++ app.py
@@ -1,43 +1,66 @@
 import sys
+import os.path
 from os import walk
 import imghdr
 import csv
 import argparse
+import re
 
 from flask import Flask, redirect, url_for, request
 from flask import render_template
 from flask import send_file
 
-
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['CURFIL'] = ""
+
+def writeLabels():
+    image = app.config["FILES"][app.config["HEAD"]]
+    with open(app.config["OUTDIR"]+image.replace('jpg','txt'),'w') as f:
+        for label in app.config["LABELS"]:
+            if label["name"] == "":continue
+            trunc = "0.0"
+            if "-trunc-" in label["name"]:
+                trunc = re.sub('.*-','',label["name"])
+                label["name"] = re.sub('-.*','',label["name"])
+            f.write(label["name"] + " " +
+            trunc + " "
+            "0 0.0 " +
+            str(round(float(label["xMin"]))) + " " +
+            str(round(float(label["xMax"]))) + " " +
+            str(round(float(label["yMin"]))) + " " +
+            str(round(float(label["yMax"]))) + " " +
+            "0.0 0.0 0.0 0.0 0.0 0.0 0.0\n")
+    f.close()
 
 @app.route('/tagger')
 def tagger():
     if (app.config["HEAD"] == len(app.config["FILES"])):
         return redirect(url_for('bye'))
+    if (app.config["HEAD"] < 0):
+        app.config["HEAD"] = 0
+        return redirect(url_for('tagger'))
     directory = app.config['IMAGES']
     image = app.config["FILES"][app.config["HEAD"]]
     labels = app.config["LABELS"]
-    # [{"id":"1", "name":None, "ymin":0, "ymax":2, "xmin":0, "ymax":5},
-        # {"id":"2", "name":"image", "ymin":0, "ymax":20, "xmin":6, "ymax":50}]
+    if not image == app.config["CURFIL"]:
+        app.config["CURFIL"] = image
+        current_file = app.config["OUTDIR"]+app.config["CURFIL"].replace('jpg','txt')
+        if os.path.isfile(current_file):
+            for idx,line in enumerate(open(current_file,'r').readlines()):
+                larr = line.strip().split(" ")
+                lname = larr[0]
+                if not larr[1] == "0.0":
+                    lname = lname + "-trunc-" + larr[1]
+                app.config["LABELS"].append({"id":idx+1, "name":lname, "xMin":float(larr[4]), "xMax":float(larr[5]), "yMin":float(larr[6]), "yMax":float(larr[7])})
     not_end = not(app.config["HEAD"] == len(app.config["FILES"]) - 1)
-    print(not_end)
-    return render_template('tagger.html', not_end=not_end, directory=directory, image=image, labels=labels, head=app.config["HEAD"] + 1, len=len(app.config["FILES"]))
+    not_begin = app.config["HEAD"] > 0
+    return render_template('tagger.html', not_end=not_end, not_begin=not_begin, directory=directory, image=image, labels=labels, head=app.config["HEAD"] + 1, len=len(app.config["FILES"]))
 
 @app.route('/next')
 def next():
-    image = app.config["FILES"][app.config["HEAD"]]
+    writeLabels()
     app.config["HEAD"] = app.config["HEAD"] + 1
-    with open(app.config["OUT"],'a') as f:
-        for label in app.config["LABELS"]:
-            f.write(image + "," +
-            label["id"] + "," +
-            label["name"] + "," +
-            str(round(float(label["xMin"]))) + "," +
-            str(round(float(label["xMax"]))) + "," +
-            str(round(float(label["yMin"]))) + "," +
-            str(round(float(label["yMax"]))) + "\n")
     app.config["LABELS"] = []
     return redirect(url_for('tagger'))
 
@@ -68,21 +91,23 @@ def label(id):
     app.config["LABELS"][int(id) - 1]["name"] = name
     return redirect(url_for('tagger'))
 
-# @app.route('/prev')
-# def prev():
-#     app.config["HEAD"] = app.config["HEAD"] - 1
-#     return redirect(url_for('tagger'))
+@app.route('/prev')
+def prev():
+    writeLabels()
+    app.config["HEAD"] = app.config["HEAD"] - 1
+    app.config["LABELS"] = []
+    return redirect(url_for('tagger'))
 
 @app.route('/image/<f>')
 def images(f):
     images = app.config['IMAGES']
     return send_file(images + f)
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('dir', type=str, help='specify the images directory')
-    parser.add_argument("--out")
+    parser.add_argument("--out", help='specify labels director')
+    parser.add_argument("--port", help='specify port to run on')
     args = parser.parse_args()
     directory = args.dir
     if directory[len(directory) - 1] != "/":
@@ -99,10 +124,16 @@ if __name__ == "__main__":
     app.config["FILES"] = files
     app.config["HEAD"] = 0
     if args.out == None:
-        app.config["OUT"] = "out.csv"
+        app.config["OUTDIR"] = args.dir
     else:
-        app.config["OUT"] = args.out
+        app.config["OUTDIR"] = args.out
+    if not app.config["OUTDIR"].endswith("/"):
+        app.config["OUTDIR"] += "/"
+    if args.port == None:
+        app.config["PORT"] = 5555
+    else:
+        app.config["PORT"] = args.port
     print(files)
     with open("out.csv",'w') as f:
         f.write("image,id,name,xMin,xMax,yMin,yMax\n")
-    app.run(debug="True")
+    app.run(host='0.0.0.0',debug="True",port=int(app.config["PORT"]))

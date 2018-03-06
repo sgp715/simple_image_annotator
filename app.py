acb4bb7e86abf3b1754cf7dbb5a4f9a1d9ea0eb4
sgp715 <7sebastianperez@gmail.com>
2018-03-05 21:35:34 -0800

    starting to add support for kitti format

--- app.py
+++ app.py
@@ -1,41 +1,76 @@
-import sys
+import os.path
 from os import walk
-import imghdr
-import csv
 import argparse
+import re
 
 from flask import Flask, redirect, url_for, request
 from flask import render_template
 from flask import send_file
 
-
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['CURFIL'] = ""
+
+def writeLabelsCSV(image):
+    with open(app.config["OUT"], 'a') as f:
+        for label in app.config["LABELS"]:
+            f.write(image + "," +
+                    label["id"] + "," +
+                    label["name"] + "," +
+                    str(round(float(label["xMin"]))) + "," +
+                    str(round(float(label["xMax"]))) + "," +
+                    str(round(float(label["yMin"]))) + "," +
+                    str(round(float(label["yMax"]))) + "\n")
+
+def writeLabelsKITTI(image):
+    with open(app.config["OUT"]+re.sub('\.(jpg|jpeg|png|tif)','.txt',image),'w') as f:
+        for label in app.config["LABELS"]:
+            if label["name"] == "":continue
+            trunc = "0.0"
+            if "-trunc-" in label["name"]:
+                trunc = re.sub('.*-','',label["name"])
+                label["name"] = re.sub('-.*','',label["name"])
+            f.write(label["name"] + " " +
+            trunc + " "
+            "0 0.0 " +
+            str(round(float(label["xMin"]))) + " " +
+            str(round(float(label["yMin"]))) + " " +
+            str(round(float(label["xMax"]))) + " " +
+            str(round(float(label["yMax"]))) + " " +
+            "0.0 0.0 0.0 0.0 0.0 0.0 0.0\n")
 
 @app.route('/tagger')
 def tagger():
     if (app.config["HEAD"] == len(app.config["FILES"])):
         return redirect(url_for('bye'))
+    if (app.config["HEAD"] < 0):
+        app.config["HEAD"] = 0
+        return redirect(url_for('tagger'))
     directory = app.config['IMAGES']
     image = app.config["FILES"][app.config["HEAD"]]
     labels = app.config["LABELS"]
+    if not image == app.config["CURFIL"]:
+        app.config["CURFIL"] = image
+        current_file = app.config["OUT"]+re.sub('\.(jpg|jpeg|png|tif)','.txt',app.config["CURFIL"])
+        if os.path.isfile(current_file):
+            for idx,line in enumerate(open(current_file,'r').readlines()):
+                larr = line.strip().split(" ")
+                lname = larr[0]
+                if not larr[1] == "0.0":
+                    lname = lname + "-trunc-" + larr[1]
+                app.config["LABELS"].append({"id":idx+1, "name":lname, "xMin":float(larr[4]), "yMin":float(larr[5]), "xMax":float(larr[6]), "yMax":float(larr[7])})
     not_end = not(app.config["HEAD"] == len(app.config["FILES"]) - 1)
-    print(not_end)
-    return render_template('tagger.html', not_end=not_end, directory=directory, image=image, labels=labels, head=app.config["HEAD"] + 1, len=len(app.config["FILES"]))
+    not_begin = app.config["HEAD"] > 0
+    return render_template('tagger.html', not_end=not_end, not_begin=not_begin, directory=directory, image=image, labels=labels, head=app.config["HEAD"] + 1, len=len(app.config["FILES"]))
 
 @app.route('/next')
 def next():
     image = app.config["FILES"][app.config["HEAD"]]
+    if app.config["FORMAT"] == "KITTI":
+        writeLabelsKITTI(image)
+    else:
+        writeLabelsCSV(image)
     app.config["HEAD"] = app.config["HEAD"] + 1
-    with open(app.config["OUT"],'a') as f:
-        for label in app.config["LABELS"]:
-            f.write(image + "," +
-            label["id"] + "," +
-            label["name"] + "," +
-            str(round(float(label["xMin"]))) + "," +
-            str(round(float(label["xMax"]))) + "," +
-            str(round(float(label["yMin"]))) + "," +
-            str(round(float(label["yMax"]))) + "\n")
     app.config["LABELS"] = []
     return redirect(url_for('tagger'))
 
@@ -66,21 +101,28 @@ def label(id):
     app.config["LABELS"][int(id) - 1]["name"] = name
     return redirect(url_for('tagger'))
 
-# @app.route('/prev')
-# def prev():
-#     app.config["HEAD"] = app.config["HEAD"] - 1
-#     return redirect(url_for('tagger'))
+@app.route('/prev')
+def prev():
+    image = app.config["FILES"][app.config["HEAD"]]
+    if app.config["FORMAT"] == "KITTI":
+        writeLabelsKITTI(image)
+    else:
+        writeLabelsCSV(image)
+    app.config["HEAD"] = app.config["HEAD"] - 1
+    app.config["LABELS"] = []
+    return redirect(url_for('tagger'))
 
 @app.route('/image/<f>')
 def images(f):
     images = app.config['IMAGES']
     return send_file(images + f)
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('dir', type=str, help='specify the images directory')
-    parser.add_argument("--out")
+    parser.add_argument("--out", help='specify labels director')
+    parser.add_argument("--port", help='specify port to run on')
+    parser.add_argument("-k", "--kitti", help='specify that you want the output format to be KITTI', action="store_true")
     args = parser.parse_args()
     directory = args.dir
     if directory[len(directory) - 1] != "/":
@@ -96,11 +138,25 @@ if __name__ == "__main__":
         exit()
     app.config["FILES"] = files
     app.config["HEAD"] = 0
-    if args.out == None:
-        app.config["OUT"] = "out.csv"
+    if args.kitti != None:
+        app.config["FORMAT"] = "KITTI"
+        if args.out == None:
+            app.config["OUT"] = args.dir
+        else:
+            app.config["OUT"] = args.out
+        if not app.config["OUT"].endswith("/"):
+            app.config["OUT"] += "/"
     else:
-        app.config["OUT"] = args.out
+        app.config["FORMAT"] = "CSV"
+        if args.out == None:
+            app.config["OUT"] = "out.csv"
+        else:
+            app.config["OUT"] = args.out
+    if args.port == None:
+        app.config["PORT"] = 5555
+    else:
+        app.config["PORT"] = args.port
     print(files)
     with open("out.csv",'w') as f:
         f.write("image,id,name,xMin,xMax,yMin,yMax\n")
-    app.run(debug="True")
+    app.run(host='0.0.0.0',debug="True",port=int(app.config["PORT"]))
